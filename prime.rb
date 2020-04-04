8f0dd10ee0d6e66253eb8f4d885b3bd267dfd22f
Hannah Gouin <hannah.l.gouin@gmail.com>
2020-04-04 03:54:20 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,8 @@
 def prime? (num)
+  return false if num < 1
   (2..(num - 1)).each do |n|
     return false if num % n == 0
   end
-  ((-num + 1)..-2).each do |n|
-    return false if num % n == 0
-  end
+
   true
 end
